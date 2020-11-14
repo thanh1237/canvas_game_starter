0f30d00eb2da6cd61a7103d2b62d050a1053153f
Nguyen Hoang Thanh <nhthanh100498@gmail.com>
2020-11-14 21:23:29 +0700

    change

--- game.js
+++ game.js
@@ -9,15 +9,14 @@ Here, we create and add our "canvas" to the page.
 We also load all of our images. 
 */
 
-
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+canvas.width = 1244;
+canvas.height = 700;
 document.body.appendChild(canvas);
 
-let bgReady, heroReady, monsterReady;
-let bgImage, heroImage, monsterImage;
+let bgReady, aimPointReady, monsterReady, fireReady, shootReady;
+let bgImage, aimPointImage, monsterImage, fireImage, shootImage;
 
 let startTime = Date.now();
 const SECONDS_PER_ROUND = 30;
@@ -29,129 +28,142 @@ function loadImages() {
     // show the background image
     bgReady = true;
   };
-  bgImage.src = "images/background.png";
-  heroImage = new Image();
-  heroImage.onload = function () {
-    // show the hero image
-    heroReady = true;
+  bgImage.src = "images/maxresdefault.jpg";
+  shootImage = new Image();
+  shootImage.onload = function () {
+    // show the background image
+    shootReady = false;
   };
-  heroImage.src = "images/hero.png";
+  shootImage.src = "images/img3.png";
+  fireImage = new Image();
+  fireImage.onload = function () {
+    // show the background image
+    fireReady = false;
+  };
+  fireImage.src = "images/fire3.png";
+  aimPointImage = new Image();
+  aimPointImage.onload = function () {
+    // show the aimPoint image
+    aimPointReady = true;
+  };
+  aimPointImage.src = "images/Picture1-removebg-preview.png";
 
   monsterImage = new Image();
   monsterImage.onload = function () {
     // show the monster image
     monsterReady = true;
   };
-  monsterImage.src = "images/monster.png";
+  monsterImage.src = "images/halo-removebg-preview.png";
 }
 
-/** 
+/**
  * Setting up our characters.
- * 
+ *
  * Note that heroX represents the X position of our hero.
  * heroY represents the Y position.
  * We'll need these values to know where to "draw" the hero.
- * 
+ *
  * The same applies to the monster.
  */
 
-let heroX = canvas.width / 2;
-let heroY = canvas.height / 2;
+let aimPointX = canvas.width / 2;
+let aimPointY = canvas.height / 2;
 
-let monsterX = 100;
-let monsterY = 100;
+let monsterX = Math.floor(Math.random() * canvas.width) - 600;
+let monsterY = 175 + Math.floor(Math.random() * canvas.height) / 2;
 
-/** 
- * Keyboard Listeners
- * You can safely ignore this part, for now. 
- * 
- * This is just to let JavaScript know when the user has pressed a key.
-*/
+var shootSound;
+var d = aimPointX - monsterX;
+var f = aimPointY - monsterY;
+
 let keysPressed = {};
 function setupKeyboardListeners() {
   // Check for keys pressed where key represents the keycode captured
-  // For now, do not worry too much about what's happening here. 
-  document.addEventListener("keydown", function (e) {
-    keysPressed[e.key] = true;
-  }, false);
-
-  document.addEventListener("keyup", function (e) {
-    keysPressed[e.key] = false;
-  }, false);
+  // For now, do not worry too much about what's happening here.
+  document.addEventListener("mousedown", shoot);
+  document.addEventListener("mouseup", stopShoot);
+  document.addEventListener("mousemove", mouseMoveHandler, false);
 }
 
+function shoot() {
+  fireReady = true;
+  shootReady = true;
+  playAudio();
+  if (d < 25 || f < 40) {
+    monsterReady = false;
+  }
+}
+function stopShoot() {
+  fireReady = false;
+  shootReady = false;
+  if ((monsterReady = false)) {
+    monsterReady = true;
+  }
+}
+function mouseMoveHandler(e) {
+  var relativeX = e.clientX - canvas.offsetLeft;
+  if (relativeX > 0 && relativeX < canvas.width) {
+    aimPointX = relativeX - 17;
+  }
+  var relativeY = e.clientY - canvas.offsetLeft;
+  if (relativeY > 0 && relativeY < canvas.height) {
+    aimPointY = relativeY - 15;
+  }
+}
+var audioType;
+var audio = new Audio();
+if (audio.canPlayType("audio/mp3")) {
+  audioType = ".mp3";
+} else {
+  audioType = ".wav";
+}
 
-/**
- *  Update game objects - change player position based on key pressed
- *  and check to see if the monster has been caught!
- *  
- *  If you change the value of 5, the player will move at a different rate.
- */
-let update = function () {
-  // Update the time.
-  elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+//Function to play the exact file format
+function playAudio() {
+  var audio = new Audio("sound/shoot" + audioType);
+  audio.play();
+}
+function update() {
+  if ((dead = true)) {
+    monsterReady = true;
+  }
+}
 
-  if (keysPressed["ArrowUp"]) {
-    heroY -= 5;
-  }
-  if (keysPressed["ArrowDown"]) {
-    heroY += 5;
-  }
-  if (keysPressed["ArrowLeft"]) {
-    heroX -= 5;
-  }
-  if (keysPressed["ArrowRight"]) {
-    heroX += 5;
-  }
-
-  // Check if player and monster collided. Our images
-  // are 32 pixels big.
-  if (
-    heroX <= (monsterX + 32)
-    && monsterX <= (heroX + 32)
-    && heroY <= (monsterY + 32)
-    && monsterY <= (heroY + 32)
-  ) {
-    // Pick a new location for the monster.
-    // Note: Change this to place the monster at a new, random location.
-    monsterX = monsterX + 50;
-    monsterY = monsterY + 70;
-  }
-};
-
-/**
- * This function, render, runs as often as possible.
- */
 function render() {
   if (bgReady) {
     ctx.drawImage(bgImage, 0, 0);
   }
-  if (heroReady) {
-    ctx.drawImage(heroImage, heroX, heroY);
-  }
   if (monsterReady) {
     ctx.drawImage(monsterImage, monsterX, monsterY);
+    {
+      if (monsterX < 0) {
+        monsterX = 49;
+      }
+    }
   }
-  ctx.fillText(`Seconds Remaining: ${SECONDS_PER_ROUND - elapsedTime}`, 20, 100);
-};
+  if (aimPointReady) {
+    ctx.drawImage(aimPointImage, aimPointX, aimPointY);
+  }
+  if (shootReady) {
+    ctx.drawImage(shootImage, aimPointX - 10, aimPointY - 5);
+  }
+  if (fireReady) {
+    ctx.drawImage(fireImage, 625, 350);
+  }
+}
 
-/**
- * The main game loop. Most every game will have two distinct parts:
- * update (updates the state of the game, in this case our hero and monster)
- * render (based on the state of our game, draw the right things)
- */
 function main() {
-  update(); 
+  update();
   render();
-  // Request to do this again ASAP. This is a special method
-  // for web browsers. 
   requestAnimationFrame(main);
-};
+}
 
-// Cross-browser support for requestAnimationFrame.
-// Safely ignore this line. It's mostly here for people with old web browsers.
 var w = window;
-requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
+requestAnimationFrame =
+  w.requestAnimationFrame ||
+  w.webkitRequestAnimationFrame ||
+  w.msRequestAnimationFrame ||
+  w.mozRequestAnimationFrame;
 
 // Let's play this game!
 loadImages();
